95ca2d6d6a1e7adb24b88e7706d4f32a27dabc66
foridislam332 <foridislam991@gmail.com>
2023-05-03 12:21:43 +0600

    scroll to top done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 const Main = () => {
     return (
         <>
+            <ScrollToTop />
+
             <Navbar />
 
             <main>
