54c274f03a1cffb1facb418f0fef3f08ef7fc0d3
foridislam332 <foridislam991@gmail.com>
2023-05-04 17:55:37 +0600

    Environment variable added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDQ7CDpEbiLdfxyc6Z5c_kPcbGykwDyh8I",
-    authDomain: "tasty-salt.firebaseapp.com",
-    projectId: "tasty-salt",
-    storageBucket: "tasty-salt.appspot.com",
-    messagingSenderId: "859049976219",
-    appId: "1:859049976219:web:bd29ef5015fcad14f7c13e"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
