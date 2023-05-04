d19b75a238aa05aef08eba0852490772f0c8428a
foridislam332 <foridislam991@gmail.com>
2023-05-04 19:34:23 +0600

    blog update

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -13,22 +13,26 @@ const Blogs = () => {
                     <div className='shadow-xl p-4 rounded-lg hover:shadow-[#f891236c] transition-all duration-300 ease-in-out'>
                         <h2 className='text-2xl mb-2'><span className='font-semibold'>Question : </span> Tell us the differences between uncontrolled and controlled components.</h2>
 
-                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> Tell us the differences between uncontrolled and controlled components.Tell us the differences between uncontrolled and controlled components.</p>
+                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> In the context of web development, uncontrolled and controlled components refer to different approaches for managing form inputs in React.js.</p>
                     </div>
                     <div className='shadow-xl p-4 rounded-lg hover:shadow-[#f891236c] transition-all duration-300 ease-in-out'>
-                        <h2 className='text-2xl mb-2'><span className='font-semibold'>Question : </span> Tell us the differences between uncontrolled and controlled components.</h2>
+                        <h2 className='text-2xl mb-2'><span className='font-semibold'>Question : </span> How to validate React props using PropTypes?</h2>
 
-                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> Tell us the differences between uncontrolled and controlled components.Tell us the differences between uncontrolled and controlled components.</p>
+                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> React provides a package called "PropTypes" that can be used to validate the props of the component. PropTypes are used to define the types and shape of a component expects.</p>
                     </div>
                     <div className='shadow-xl p-4 rounded-lg hover:shadow-[#f891236c] transition-all duration-300 ease-in-out'>
-                        <h2 className='text-2xl mb-2'><span className='font-semibold'>Question : </span> Tell us the differences between uncontrolled and controlled components.</h2>
+                        <h2 className='text-2xl mb-2'><span className='font-semibold'>Question : </span> Tell us the difference between nodejs and express js.</h2>
 
-                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> Tell us the differences between uncontrolled and controlled components.Tell us the differences between uncontrolled and controlled components.</p>
+                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> Node.js and Express.js are both widely used technologies in web development, but they serve different purposes.</p>
+
+                        <p className='my-2'>Node.js is a runtime environment for executing JavaScript code on the server side.</p>
+
+                        <p>Express.js, on the other hand, is a web framework built on top of Node.js.</p>
                     </div>
                     <div className='shadow-xl p-4 rounded-lg hover:shadow-[#f891236c] transition-all duration-300 ease-in-out'>
-                        <h2 className='text-2xl mb-2'><span className='font-semibold'>Question : </span> Tell us the differences between uncontrolled and controlled components.</h2>
+                        <h2 className='text-2xl mb-2'><span className='font-semibold'>Question : </span> What is a custom hook, and why will you create a custom hook?</h2>
 
-                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> Tell us the differences between uncontrolled and controlled components.Tell us the differences between uncontrolled and controlled components.</p>
+                        <p> <span className='text-secondary font-semibold text-xl'>Answer : </span> A custom hook is a reusable function in React that logic and can be shared between components. Custom hooks are a way to extract repeated logic from components and reuse it in a more concise and reusable way. Custom hooks are created using the same syntax as regular hooks, but with a custom name and implementation..</p>
                     </div>
                 </div>
             </div>
